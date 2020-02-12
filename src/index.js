723b3013d7a48cf2742c2d7ce7c0bf8646134502
Shawn DelPercio <sdelperc@gmail.com>
2020-02-12 17:48:56 -0500

    added pagination functionality

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import Charts from './components/Charts';
+import Navbar from './components/Navbar';
+import Forms from './components/Forms';
 
-import "./styles.scss";
+import './styles.scss';
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
+	const [coinData, setCoinData] = useState([]);
+	const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
-    </div>
-  );
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=${page}&sparkline=true`
+			)
+			.then(res => {
+				setCoinData(res.data);
+			})
+			.catch(err => console.log(err));
+	}, [page]);
+
+	const selectAPage = event => {
+		setPage(Number(event.target.value));
+	};
+
+	return (
+		<div className='App'>
+			<Navbar />
+			<Forms selectAPage={selectAPage} />
+			<Charts coinData={coinData} />
+		</div>
+	);
 };
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
